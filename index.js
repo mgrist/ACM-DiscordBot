74b59fd9c8d84989cac8a8b0e07e669b4cfae814
mgrist <mgrist@angelo.edu>
2022-05-17 01:00:28 -0500

    now looks for command in specific text channel

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, "GUILDS", "GUILD_MESSAGES"],
 });
 
+// Text channel ID that role commands will be issued in
+const roleChannel = "975972573164761108";
+
 // ID number for each role in the server.
-const president = "975921199743901716";
-const vicePresident = "975921252181098506";
-const officer = "975921128382021703";
-const eventsTeam = "975920858067533855";
-const member = "975920537073225789";
-const tutor = "975921305759137823";
-const student = "975920815545659432";
+const president_role = "975921199743901716";
+const vicePresident_role = "975921252181098506";
+const officer_role = "975921128382021703";
+const eventsTeam_role = "975920858067533855";
+const member_role = "975920537073225789";
+const tutor_role = "975921305759137823";
+const student_role = "975920815545659432";
 
 // When the client is ready, run this code (only once)
 client.once("ready", (c) => {
@@ -24,8 +27,10 @@ client.once("ready", (c) => {
 
 // When client sends a message, do something
 client.on("messageCreate", async (message) => {
-	if (message.content === "!hello") {
-		console.log(await message.guild.roles.fetch());
+	if (message.channelId === roleChannel) {
+		if (message.content === "!hello") {
+			console.log(await message.guild.channels.fetch());
+		}
 	}
 });
 
