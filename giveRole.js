ca3ac282f8144583529c54eedaa3956e01705130
mgrist <mgrist@angelo.edu>
2022-05-23 22:20:17 -0500

    bot now sends role request to mods

--- giveRole.js
+++ giveRole.js
@@ -1,4 +1,9 @@
-const { roleChannel, generalChannel } = require("./config.json");
+const {
+	roleChannel,
+	generalChannel,
+	prefix,
+	adminList,
+} = require("./config.json");
 
 const giveRoleOnReaction = async (reaction, user, rolesArr, client) => {
 	// If an acutal user is reacting, not a bot.
@@ -31,13 +36,62 @@ async function sendRoleRequest(id, client) {
 	const user = await client.users.fetch(id);
 
 	// if DMs are blocked, message them in general, otherwise send a DM to the mods.
-	await user.send("Please enter the role name desired").catch(() => {
-		client.channels.cache
-			.get(generalChannel)
-			.send(
-				`Hi ${user}, you requested a custom role but your DMs are closed. Please open your DMs.`
-			);
+	await user
+		.send(
+			"Respond with `!role` followed by the desired role name, no spaces allowed in the role name"
+		)
+		.catch(() => {
+			client.channels.cache
+				.get(generalChannel)
+				.send(
+					`Hi ${user}, you requested a custom role but your DMs are closed. Please open your DMs.`
+				);
+		});
+
+	// when the bot recieves a dm from the author of the reaction, parse message and send request to admins
+	client.on("messageCreate", async (message) => {
+		if (
+			message.channel.type === "DM" &&
+			message.author.id === id &&
+			!message.author.bot
+		) {
+			// grab the custom role name that the user entered
+			let roleName = parseRoleName(message);
+
+			// send all mods a dm to give custom role to user
+			for (const adminID of adminList) {
+				const admin = await client.users.fetch(adminID);
+				await admin.send(
+					message.author.username +
+						"#" +
+						message.author.discriminator +
+						" has requested the role `" +
+						roleName +
+						"`"
+				);
+			}
+
+			// verify that the role request is submitted
+			message.author.send("You're role request has been submitted.");
+		}
 	});
+}
+
+function parseRoleName(message) {
+	// if message does not start with ! prefix, or author is a bot, ignore.
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	// checks if given a role command and an argument
+	if (command === "role" && args[0]) {
+		return args[0];
+	} else {
+		message.author.send(
+			"Invalid command. Make sure to use !role and provide an argument."
+		);
+	}
 }
 
 module.exports = {
